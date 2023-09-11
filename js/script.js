2ed108989cd8501df28c4a7ef20c65063bc22da8
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2023-09-11 21:23:41 +0500

    task one completed

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ menuLink.forEach(link => {
         menuLinks.classList.remove("active");
     })  
 });
+
+// making background color of nabar white on scroll
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    
+    if(window.scrollY > 0){
+      header.classList.add('scrolled');
+    }else {
+      header.classList.remove('scrolled');
+    }
+})
+
+// time and day
+function displayTimeAndDay() {
+    const date = new Date();
+    const timeInMS = date.getTime();
+
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const currentDay = days[date.getDay()];
+    
+    document.querySelector("#current-time").innerHTML = "" + timeInMS;
+    document.querySelector("#current-day").innerHTML = "" + currentDay;
+
+    console
+
+}
+
+setInterval(displayTimeAndDay, 1000);
+
