c690ad08b1f2694cad59ec5d3586e4feba1401d2
GreyyDaze <117322834+GreyyDaze@users.noreply.github.com>
2023-09-12 15:00:49 +0500

    current time code updated.

--- js/script.js
+++ js/script.js
@@ -10,55 +10,58 @@ menuBtn.addEventListener("click", () => {
 // close the menu on link clink
 const menuLink = document.querySelectorAll(".menu-links a");
 
-menuLink.forEach(link => {
-    link.addEventListener("click", () => {
-        menuLinks.classList.remove("active");
-    })  
+menuLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuLinks.classList.remove("active");
+  });
 });
 
 // making background color of nabar white on scroll
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
-window.addEventListener('scroll', () => {
-    
-    if(window.scrollY > 0){
-      header.classList.add('scrolled');
-    }else {
-      header.classList.remove('scrolled');
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 
 // time and day
 function displayTimeAndDay() {
-    const date = new Date();
-    const timeInMS = date.getTime();
+  const date = new Date();
+  const currentTimeInMs = Date.now();
 
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const currentDay = days[date.getDay()];
-    
-    document.querySelector("#current-time").innerHTML = "" + timeInMS;
-    document.querySelector("#current-day").innerHTML = "" + currentDay;
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const currentDay = days[date.getDay()];
 
-    console
-
+  document.querySelector("#current-time").innerHTML = "" + currentTimeInMs;
+  document.querySelector("#current-day").innerHTML = "" + currentDay;
 }
 
 setInterval(displayTimeAndDay, 1000);
 
 // nav links on click
-function addActiveClass(){
+function addActiveClass() {
   const activeLink = document.activeElement;
 
-  menuLink.forEach(link => {
-    if(link.classList.contains('current-link')) {
-        link.classList.remove('current-link');
+  menuLink.forEach((link) => {
+    if (link.classList.contains("current-link")) {
+      link.classList.remove("current-link");
     }
   });
 
-  if(activeLink && activeLink.tagName === 'A') {
-      activeLink.classList.add('current-link');
-  
+  if (activeLink && activeLink.tagName === "A") {
+    activeLink.classList.add("current-link");
   }
 }
 
-document.addEventListener('click', addActiveClass);
+document.addEventListener("click", addActiveClass);
